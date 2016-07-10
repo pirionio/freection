8e11b3a2211fd0f26c69914a5b00b1ce72bb7c11
Doron Somech <somdoron@gmail.com>
2016-07-10 15:10:57 +0300

    Configure express

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,12 @@
+'use strict'
+const express = require('express')
+const path = require('path')
+
+const app = new express()
+app.set('port', (process.env.PORT || 3000))
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.listen(app.get('port'), function () {
+    console.log(`Running in ${app.get('env')} mode on port ${app.get('port')}`)
+})
 
