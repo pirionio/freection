a658717dcec9ed95779fd970017536c4057d1637
Doron Somech <somdoron@gmail.com>
2016-07-10 17:29:40 +0300

    hot reload

--- src/server/index.js
+++ src/server/index.js
@@ -9,6 +9,17 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../public')))
 app.use('/api', api)
 
+// Dev only
+if (app.get('env') === 'development') {
+    const webpack = require('webpack')
+    const webpackDevMiddleware = require('webpack-dev-middleware')
+    const webpackHotMiddleware = require('webpack-hot-middleware')
+    const webpackConfig = require('../../webpack.config')
+    const compiler = webpack(webpackConfig)
+    app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: webpackConfig.output.publicPath}))
+    app.use(webpackHotMiddleware(compiler))
+}
+
 app.set('port', (process.env.PORT || 3000))
 app.listen(app.get('port'), function () {
     console.log(`Running in ${app.get('env')} mode on port ${app.get('port')}`)
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
+const webpack = require('webpack')
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     devtool: 'inline-source-map',
     context: path.join(__dirname, '/src/app'),
-    entry: ['./index.js'],
+    entry: [
+        'webpack-hot-middleware/client',
+        './index.js'],
     output: {
         path: path.join(__dirname, '/src/public'),
         filename: 'bundle.js',
         publicPath: '/'
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
         new ExtractTextPlugin('style.css', { allChunks: true })
     ],
     module: {
@@ -19,6 +23,7 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
                 loaders: [
+                    'react-hot',
                     'babel?presets[]=react&presets[]=es2015'
                 ]
             },
