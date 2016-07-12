bac86f6a22fea45539443746846f7e06d5af72f3
Daniel <daniel.wolf@freection.com>
2016-07-12 11:02:51 +0300

    Displaying  the creation time of a Thing.

--- src/app/components/Thing/NewThing.js
+++ src/app/components/Thing/NewThing.js
@@ -1,16 +1,24 @@
 const React = require('react')
 const {Component, PropTypes} = React
+const moment = require('moment')
 
 class NewThing extends Component {
+
     render () {
         const {thing} = this.props
+        const createdAt = moment(thing.createdAt).format('DD-MM-YYYY HH:mm')
         return (
             <div className="new-thing">
-                <div className="thing-creator">
-                    {thing.creator.email}
-                </div>
-                <div className="thing-subject">
-                    {thing.subject}
+                <div className="metadata-row">
+                    <div className="thing-creator">
+                        {thing.creator.email}
+                    </div>
+                    <div className="thing-subject">
+                        {thing.subject}
+                    </div>
+                    <div className="thing-creation-time">
+                        {createdAt}
+                    </div>
                 </div>
                 <div className="thing-body">
                     {thing.body}
