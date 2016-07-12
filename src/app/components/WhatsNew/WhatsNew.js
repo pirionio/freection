a95383cc25ecb104cfad45be6247e9eec1bf934d
Daniel <daniel.wolf@freection.com>
2016-07-12 10:35:59 +0300

    Displaying newest Thing at the bottom.

--- src/app/components/WhatsNew/WhatsNew.js
+++ src/app/components/WhatsNew/WhatsNew.js
@@ -1,6 +1,8 @@
 const React = require('react')
 const {Component, PropTypes} = React
 const {connect} = require('react-redux')
+const {sortBy} = require('lodash')
+const ReactDOM = require('react-dom')
 
 const {GeneralConstants} = require('../../constants')
 const NewThing = require('../Thing/NewThing')
@@ -14,9 +16,26 @@ class WhatsNew extends Component {
         }, GeneralConstants.FETCH_INTERVAL_MILLIS)
     }
 
+    componentWillUpdate () {
+        const node = ReactDOM.findDOMNode(this)
+        this.shouldScrollBottom = (node.scrollTop + node.offsetHeight) === node.scrollHeight
+    }
+
+    componentDidUpdate () {
+        if (this.shouldScrollBottom) {
+            let node = ReactDOM.findDOMNode(this)
+            node.scrollTop = node.scrollHeight
+        }
+    }
+
+    sortThingsByDate () {
+        return sortBy(this.props.things, thing => thing.createdAt)
+    }
+
     render () {
-        const {things} = this.props
-        const rows = things && things.length ? things.map(thing => <NewThing thing={thing} key={thing.id} />) : 'There are no new Things'
+        const rows = this.props.things && this.props.things.length ?
+            this.sortThingsByDate().map(thing => <NewThing thing={thing} key={thing.id} />) :
+            'There are no new Things'
         return (
             <div className="whats-new-container">
                 <div className="whats-new-content">
