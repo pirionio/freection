798e20684b48ccb5b899b1cf12c0bb2f49fc6975
Daniel <daniel.wolf@freection.com>
2016-07-14 16:02:28 +0300

    Setting style for a selected menu option in the Side Bar.

--- src/app/components/SideBar/SideBar.js
+++ src/app/components/SideBar/SideBar.js
@@ -6,9 +6,9 @@ class SideBar extends Component {
     render() {
         return (
             <div className="side-bar">
-                <Link to="/whatsnew" className="side-bar-link">What's New?</Link>
-                <Link to="/todo" className="side-bar-link">To Do</Link>
-                <Link to="/followup" className="side-bar-link">Follow Up</Link>
+                <Link to="/whatsnew" className="side-bar-link" activeClassName="active">What's New?</Link>
+                <Link to="/todo" className="side-bar-link" activeClassName="active">To Do</Link>
+                <Link to="/followup" className="side-bar-link" activeClassName="active">Follow Up</Link>
             </div>
         )
     }
