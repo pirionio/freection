26581a193ffa671d6f8630beec877f15a0a37257
Daniel <daniel.wolf@freection.com>
2016-08-01 19:56:22 +0300

    Handle errors better - don't let them get to the client or break the server.

--- src/server/mail-fetch/index.js
+++ src/server/mail-fetch/index.js
@@ -2,6 +2,7 @@ const {chain} = require('lodash/core')
 
 const ImapConnectionPool = require('../shared/utils/imap/imap-connection-pool')
 const {emailToDto} = require('../shared/transformers')
+const logger = require('../shared/utils/logger')
 
 function establishConnection(user) {
     return ImapConnectionPool.getConnection(user)
@@ -49,18 +50,36 @@ module.exports = app => {
 
     app.get('/emails/api/unread', (request, response) => {
         const user = request.user
-        fetchUnreadMessages(user).then(emails => response.json(emails.map(emailToDto)))
+        fetchUnreadMessages(user)
+            .then(emails => response.json(emails.map(emailToDto)))
+            .catch(error => {
+                const message = 'Error while fetching unread emails'
+                logger.error(message, error)
+                response.status(500).send(message)
+            })
     })
 
     app.get('/emails/api/:emailThreadId', (request, response) => {
         const user = request.user
         const {emailThreadId} = request.params
-        fetchFullThread(user, emailThreadId).then(thread => response.json(thread))
+        fetchFullThread(user, emailThreadId)
+            .then(thread => response.json(thread))
+            .catch(error => {
+                const message = `Error while fetching thread ${emailThreadId}`
+                logger.error(message, error)
+                response.status(500).send(message)
+            })
     })
 
     app.post('/emails/api/markasread', (request, response) => {
         const user = request.user
         const {emailIds} = request.body
-        markAsRead(user, emailIds).then(() => response.json({}))
+        markAsRead(user, emailIds)
+            .then(() => response.json({}))
+            .catch(error => {
+                const message = 'Error while marking emails as read'
+                logger.error(message, error)
+                response.status(500).send(message)
+            })
     })
 }
