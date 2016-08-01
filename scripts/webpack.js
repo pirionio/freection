d44ef9cdacf07b62ce51146c8592006d6936ed31
Doron Somech <somdoron@gmail.com>
2016-08-01 16:13:25 +0300

    only build webpack on production

---
+++ scripts/webpack.js
@@ -0,0 +1,10 @@
+const process = require('process')
+
+if (process.env.NODE_ENV === 'production') {
+    const webpack = require('webpack')
+    const webpackConfig = require('../config/webpack.prod.config')
+    console.log('compiling webpack...')
+    webpack(webpackConfig, function(err, stats) {
+        console.log(stats.toString("errors-only"))
+    })
+}
