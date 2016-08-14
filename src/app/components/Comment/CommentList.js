3f56827a59147dee9601a12414f1a26bd1b19e4c
Daniel <daniel.wolf@freection.com>
2016-08-14 17:12:38 +0300

    freection-91 Message Box looks like shit
    Remove console.log

--- src/app/components/Comment/CommentList.js
+++ src/app/components/Comment/CommentList.js
@@ -9,8 +9,6 @@ class CommentList extends Component {
     componentDidMount() {
         const {comments} = this.props
 
-        console.log('comments', comments)
-
         const firstUnreadMessage = chain(comments)
             .sortBy('createdAt')
             .filter({isRead: false})
