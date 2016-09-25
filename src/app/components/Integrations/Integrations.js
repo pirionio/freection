c12b165330cfdd1d9d8230ec08309471493407d3
Daniel <daniel.wolf@freection.com>
2016-09-25 19:04:37 +0300

    freection-164 I cannot add email to my todo list
    First attempt to work against the published Chrome extension.
    This is a bit hard to test in localhost, so I go straight for prod...

--- src/app/components/Integrations/Integrations.js
+++ src/app/components/Integrations/Integrations.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react'
 import useSheet from 'react-jss'
 import Icon from 'react-fontawesome'
+import classAutobind from 'class-autobind'
 
 import Flexbox from '../UI/Flexbox'
 import Link from '../UI/Link'
 
 class Integrations extends Component {
+    constructor(props) {
+        super(props)
+        classAutobind(this, Integrations.prototype)
+    }
+
+    installChromeExtension() {
+        console.log('installChromeExtension')
+        chrome.webstore.install('', () => {
+            console.log('success')
+        },
+        error => {
+            console.log('error:', error)
+        })
+    }
+
     render() {
         const {sheet: {classes}} = this.props
 
@@ -20,6 +36,15 @@ class Integrations extends Component {
                     <Icon name="slack" className={classes.icon} />
                     Slack
                 </Link>
+                <Flexbox name="chrome-extension" className={classes.extension}>
+                    <span className={classes.title}>
+                        You can also try our Chrome extension, in order to use Freection for managing your emails:
+                    </span>
+                    <button type="button" className={classes.gmailLink} onClick={this.installChromeExtension}>
+                        <Icon name="envelope" className={classes.icon} />
+                        Gmail
+                    </button>
+                </Flexbox>
             </Flexbox>
         )
     }
@@ -34,10 +59,25 @@ const style = {
     link: {
         color: 'black',
         fontSize: '1.4em',
-        marginBottom: 15,
+        marginBottom: 15
     },
     icon: {
         marginRight: 10
+    },
+    extension: {
+        marginTop: 15
+    },
+    gmailLink: {
+        color: 'black',
+        fontSize: '1.4em',
+        margin: [15, 0, 0],
+        padding: 0,
+        textDecoration: 'underline',
+        display: 'block',
+        cursor: 'pointer',
+        background: 'none',
+        border: 'none',
+        outline: 'none'
     }
 }
 
