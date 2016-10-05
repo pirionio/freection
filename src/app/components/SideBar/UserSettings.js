163c710b2d9cc85c6f457fdb7999cdf23bcf3d88
Daniel <daniel.wolf@freection.com>
2016-10-05 16:18:23 +0300

    Style of user settings, according to the mockups.

--- src/app/components/SideBar/UserSettings.js
+++ src/app/components/SideBar/UserSettings.js
@@ -2,37 +2,94 @@ import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
 import classAutobind from 'class-autobind'
 import useSheet from 'react-jss'
+import Icon from 'react-fontawesome'
 
 import Flexbox from '../UI/Flexbox'
-import SettingsMenu from './SettingsMenu'
+import Link from '../UI/Link'
 
 class UserSettings extends Component {
     constructor(props) {
         super(props)
         classAutobind(this, UserSettings.prototype)
+
+        this.state = {
+            showMenu: false
+        }
+    }
+
+    toggleSettingsMenu() {
+        this.setState({showMenu: !this.state.showMenu})
+    }
+
+    closeSettingsMenu() {
+        this.setState({showMenu: false})
+    }
+
+    isOpen() {
+        return this.state.showMenu
+    }
+
+    getSettingMenu() {
+        const {sheet: {classes}} = this.props
+        return this.isOpen() ? (
+            <Flexbox name="settings-menu" grow={1} container="column" justifyContent="space-between" className={classes.menu}>
+                <Link to="/integrations" className={classes.menuOption} onClick={this.closeSettingsMenu}>Integrations</Link>
+                <a href="/login/logout" className={classes.menuOption}>Logout</a>
+            </Flexbox>
+        ) : null
     }
 
     render() {
         const {currentUser, sheet: {classes}} = this.props
+
+        const settingsMenu = this.getSettingMenu()
+
         return (
-            <Flexbox name="settings" container="row" justifyContent="space-around" alignItems="center" className={classes.settings}>
+            <Flexbox name="settings" container="row" justifyContent="space-between" alignItems="center" className={classes.container}>
                 <span className={classes.user}>{currentUser.firstName}</span>
-                <SettingsMenu />
+                <Icon name={this.isOpen() ? 'chevron-down' : 'chevron-up'} onClick={this.toggleSettingsMenu} className={classes.menuButton} />
+                {settingsMenu}
             </Flexbox>
         )
     }
 }
 
 const style = {
-    settings: {
-        height: 74,
-        width: 185,
-        margin: [0, 19],
-        borderTop: '1px solid #232e34',
+    container: {
+        position: 'relative',
+        height: 60,
+        width: '100%',
+        padding: [0, 27],
+        backgroundColor: '#242f35',
         color: 'white'
     },
     user: {
-        letterSpacing: '0.1em'
+        letterSpacing: '0.1em',
+        textTransform: 'uppercase'
+    },
+    menuButton: {
+        marginRight: 10,
+        fontSize: '0.9em',
+        cursor: 'pointer'
+    },
+    menu: {
+        position: 'absolute',
+        width: '100%',
+        height: 90,
+        top: -90,
+        left: 0,
+        padding: [23, 27],
+        backgroundColor: '#2a373f'
+    },
+    menuOption: {
+        width: '100%',
+        cursor: 'pointer',
+        color: '#959a9d',
+        textDecoration: 'none',
+        letterSpacing: '0.01em',
+        '&:hover': {
+            color: 'white'
+        }
     }
 }
 
