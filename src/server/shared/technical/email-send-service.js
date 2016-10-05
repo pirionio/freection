b20b193b5ee5d997098d360c2d5ea3db8566f45c
Doron Somech <somdoron@gmail.com>
2016-10-05 16:49:38 +0300

    change sending mechanism to GMAIL API instead of SMTP

--- src/server/shared/technical/email-send-service.js
+++ src/server/shared/technical/email-send-service.js
@@ -1,10 +1,51 @@
-import * as GoogleSmtpConnectionPool from '../utils/smtp/google-smtp-connection-pool'
+//import * as GoogleSmtpConnectionPool from '../utils/smtp/google-smtp-connection-pool'
+import google from 'googleapis'
+import mailcomposer from 'mailcomposer'
+import base64url from 'base64url'
+
+import {User} from '../models'
+import config from '../config/google-oauth'
+import promisify from '../utils/promisify.js'
+
+const OAuth2 = google.auth.OAuth2
+const gmail = google.gmail('v1')
+promisify(gmail.users.messages, ['send'])
+
+async function getFullUser(user) {
+    if (user.refreshToken)
+        return user
+
+    return await User.get(user.id).run()
+}
+
+async function getAuth(user) {
+    const oauth2 = new OAuth2(config.clientID, config.clientSecret)
+    promisify(oauth2, ['getAccessToken'])
+    oauth2.setCredentials({refresh_token: user.refreshToken})
+    await oauth2.getAccessTokenAsync()
+    return oauth2
+}
+
+async function getRaw(message) {
+    const mail = mailcomposer(message)
+    promisify(mail, ['build'])
+
+    return base64url(await mail.buildAsync())
+}
+
+async function gmailSend(auth, raw) {
+    await gmail.users.messages.sendAsync({
+        userId: 'me',
+        auth: auth,
+        resource: {
+            raw
+        }
+    })
+}
 
 export async function sendMessage(user, message) {
-    const connection = await GoogleSmtpConnectionPool.getConnection(user)
-    try {
-        await connection.sendMessage(message)
-    } finally {
-        GoogleSmtpConnectionPool.releaseConnection(user, connection)
-    }
+    const raw = await getRaw(message)
+    const fullUser = await getFullUser(user)
+    const auth = await getAuth(fullUser)
+    await gmailSend(auth, raw)
 }
