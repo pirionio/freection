c9c1927713254d8a41f4dd89f50555d6cce7bc55
Daniel <daniel.wolf@freection.com>
2016-11-13 08:28:47 +0200

    Bug fix - could not Do thing from Gmail.
    Due to missing device type in analytics.

--- src/server/api/routes/email-thing-route.js
+++ src/server/api/routes/email-thing-route.js
@@ -1,18 +1,27 @@
 import {Router} from 'express'
+import {find, toArray} from 'lodash'
 
 import * as EndpointUtil from '../../shared/utils/endpoint-util'
 import * as EmailService from '../../shared/application/email-service'
+import DeviceType from '../../../common/enums/device-types'
 
 const router = Router()
 
 router.post('/', (request, response) => {
-    EndpointUtil.handlePost(request, response, EmailService.newEmailThing, {
-        body: ['emailData', 'isHex'],
-        result: false,
-        errorTemplates: {
-            general: 'Error while creating a thing from email'
+    EndpointUtil.handlePost(request, response, (user, emailData, isHex) => {
+        return EmailService.newEmailThing(user, emailData, isHex, {sourceDevice: findDevice(request.device.type)})
+    }, {
+            body: ['emailData', 'isHex'],
+            result: false,
+            errorTemplates: {
+                general: 'Error while creating a thing from email'
+            }
         }
-    })
+    )
 })
+
+function findDevice(deviceType) {
+    return find(toArray(DeviceType), {expressDeviceType: deviceType})
+}
 
 export default router
