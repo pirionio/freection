e49d8cdc4a91cf233d7492d821dc58fd0085b496
Daniel <daniel.wolf@freection.com>
2016-11-15 13:24:58 +0200

    Adding desktop notification icons.
    I try them with colors right now, that should match the status colors of the Notifications page.

--- src/app/services/desktop-notification-service.js
+++ src/app/services/desktop-notification-service.js
@@ -1,9 +1,14 @@
 import remove from 'lodash/remove'
 
 import EventTypes from '../../common/enums/event-types'
-import Logo from '../static/logo-black.png'
+import LogoNew from '../static/desktop-notification-new.png'
+import LogoInProgress from '../static/desktop-notification-inprogress.png'
+import LogoDone from '../static/desktop-notification-done.png'
+import LogoDismissed from '../static/desktop-notification-dismissed.png'
+import LogoClosed from '../static/desktop-notification-closed.png'
 import EntityTypes from '../../common/enums/entity-types.js'
 import DeviceType from '../../common/enums/device-types'
+import EventType from '../../common/enums/event-types'
 import ThingSource from '../../common/enums/thing-source'
 
 const isNotificationEnabled = 'Notification' in window
@@ -63,13 +68,33 @@ export function handleEvent(event) {
     }
 }
 
-function showNotification(id, title, body) {
+function getLogoByEventType(type) {
+    switch (type.key) {
+        case EventType.CREATED.key:
+        case EventType.SENT_BACK.key:
+            return LogoNew
+        case EventType.COMMENT.key:
+        case EventType.PING.key:
+        case EventType.PONG.key:
+            return LogoInProgress
+        case EventType.DONE.key:
+            return LogoDone
+        case EventType.DISMISSED.key:
+            return LogoDismissed
+        case EventType.CLOSED.key:
+            return LogoClosed
+        default:
+            return LogoInProgress
+    }
+}
+
+function showNotification(id, title, body, type) {
     if (notificationStore.includes(id))
         return
 
     const notification = new Notification(title, {
         body: body,
-        icon: Logo
+        icon: getLogoByEventType(type)
     })
 
     notificationStore.push = id
@@ -82,9 +107,9 @@ function showNotification(id, title, body) {
 
 function fromSlackCreateNotification(event) {
     if (event.thing.isFollowUper)
-        showNotification(event.id, 'Thing from slack added to your Follow Up list', event.thing.subject)
+        showNotification(event.id, 'Thing from slack added to your Follow Up list', event.thing.subject, event.eventType)
     else if (event.thing.isDoer)
-        showNotification(event.id, 'Thing from slack added to your To Do list list', event.thing.subject)
+        showNotification(event.id, 'Thing from slack added to your To Do list list', event.thing.subject, event.eventType)
 }
 
 function emailThingCreateNotification(event) {
@@ -93,44 +118,44 @@ function emailThingCreateNotification(event) {
         .map(recipient => recipient.name)
         .join(', ')
 
-    showNotification(event.id, `Email from ${recipientNames} added to your To Do list`, event.thing.subject)
+    showNotification(event.id, `Email from ${recipientNames} added to your To Do list`, event.thing.subject, event.eventType)
 }
 
 function fromMobileCreateNotification(event) {
     if (event.thing.isFollowUper)
-        showNotification(event.id, 'Thing from your mobile added to your Follow Up list', event.thing.subject)
+        showNotification(event.id, 'Thing from your mobile added to your Follow Up list', event.thing.subject, event.eventType)
     else if (event.thing.isDoer)
-        showNotification(event.id, 'Thing from your mobile added to your To Do list', event.thing.subject)
+        showNotification(event.id, 'Thing from your mobile added to your To Do list', event.thing.subject, event.eventType)
 }
 
 function createNotification(event) {
-    showNotification(event.id, `${event.creator.displayName} sent you a thing`, event.thing.subject)
+    showNotification(event.id, `${event.creator.payload.firstName} sent you a thing`, event.thing.subject, event.eventType)
 }
 
 function doneNotification(event) {
-    showNotification(event.id, `${event.creator.displayName} completed a thing`, event.thing.subject)
+    showNotification(event.id, `${event.creator.payload.firstName} completed a thing`, event.thing.subject, event.eventType)
 }
 
 function dismissNotification(event) {
-    showNotification(event.id, `${event.creator.displayName} dismissed a thing`, event.thing.subject)
+    showNotification(event.id, `${event.creator.payload.firstName} dismissed a thing`, event.thing.subject, event.eventType)
 }
 
 function closedNotification(event) {
-    showNotification(event.id, `${event.creator.displayName} closed a thing`, event.thing.subject)
+    showNotification(event.id, `${event.creator.payload.firstName} closed a thing`, event.thing.subject, event.eventType)
 }
 
 function pingNotification(event) {
-    showNotification(event.id, `${event.creator.displayName} pinged you`, event.thing.subject)
+    showNotification(event.id, `${event.creator.payload.firstName} pinged you`, event.thing.subject, event.eventType)
 }
 
 function pongNotification(event) {
-    showNotification(event.id, `${event.creator.displayName} ponged ${event.thing.isFollowUper ? 'you' : ''}`, event.thing.subject)
+    showNotification(event.id, `${event.creator.payload.firstName} ponged ${event.thing.isFollowUper ? 'you' : ''}`, event.thing.subject, event.eventType)
 }
 
 function sendBackNotification(event) {
-    showNotification(event.id, `${event.creator.displayName} sent a thing back`, event.thing.subject)
+    showNotification(event.id, `${event.creator.payload.firstName} sent a thing back`, event.thing.subject, event.eventType)
 }
 
 function mentionedNotification(event) {
-    showNotification(event.id, `${event.creator.displayName} mentioned you`, event.thing.subject)
+    showNotification(event.id, `${event.creator.payload.firstName} mentioned you`, event.thing.subject, event.eventType)
 }
