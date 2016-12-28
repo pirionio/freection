6d56c6b77611aa1722063fb19c8740d1d707842e
Daniel <daniel.wolf@freection.com>
2016-12-28 20:57:44 +0200

    231407833979379 Onboarding - welcome wizard
    Finish the last step of the How To.
    Right now it's only some text letting the users know they should pay attention to the Freection Bot.

--- src/app/components/Welcome/Steps/WelcomeHowto.js
+++ src/app/components/Welcome/Steps/WelcomeHowto.js
@@ -18,13 +18,44 @@ class WelcomeHowto extends Component {
 
         return (
             <WelcomeStep title="How to use Freection">
-                How To
+                <Flexbox name="explanation" className={classes.explanation}>
+                    <Flexbox container="row" alignItems="center" className={classes.row}>
+                        <img src={SingleStar} className={classes.bullet} />
+                        <span>You are almost done!</span>
+                    </Flexbox>
+                    <Flexbox container="row" alignItems="center" className={classes.row}>
+                        <img src={SingleStar} className={classes.bullet} />
+                        <span>The best place to start is with the Getting Started message you will get from our Freection Bot.</span>
+                    </Flexbox>
+                    <Flexbox container="row" alignItems="center" className={classes.row}>
+                        <img src={SingleStar} className={classes.bullet} />
+                        <span>You will find it in your Freection Inbox. Just click on the message to learn how to use Freection.</span>
+                    </Flexbox>
+                </Flexbox>
             </WelcomeStep>
         )
     }
 }
 
 const style = {
+    explanation: {
+        marginTop: 55,
+        padding: [0, 130],
+        color: 'white',
+        fontSize: '1.571em',
+        fontWeight: '300',
+        letterSpacing: '0.025em',
+        lineHeight: '1.5'
+    },
+    row: {
+        display: 'block',
+        marginBottom: 50
+    },
+    bullet: {
+        height: 15,
+        width: 15,
+        marginRight: 20
+    }
 }
 
 WelcomeHowto.propTypes = {
