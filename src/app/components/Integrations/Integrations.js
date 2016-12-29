fec3b47ab3389f505f7dd42ed30c3f1a0f572e87
Daniel <daniel.wolf@freection.com>
2016-12-29 09:49:08 +0200

    231407833979379 Onboarding - welcome wizard
    Apply the Integrations Box to the Integrations page in the app itself, after the welcome.

--- src/app/components/Integrations/Integrations.js
+++ src/app/components/Integrations/Integrations.js
@@ -1,69 +1,18 @@
 import React, {Component} from 'react'
-import useSheet from 'react-jss'
-import Icon from 'react-fontawesome'
-import classAutobind from 'class-autobind'
 
 import Flexbox from '../UI/Flexbox'
-import Link from '../UI/Link'
-import AsanaLogoBlack from '../../static/AsanaLogoBlack.png'
-import TrelloLogoBlack from '../../static/TrelloLogoBlack.png'
+import IntegrationBox from './IntegrationsBox'
 
 class Integrations extends Component {
-    constructor(props) {
-        super(props)
-        classAutobind(this, Integrations.prototype)
-    }
-
     render() {
-        const {sheet: {classes}} = this.props
+        const {location} = this.props
 
         return (
-            <Flexbox name="integrations-container" grow={1} container="column">
-                <span className={classes.title}>These are the integrations we offer right now:</span>
-                <Link to="/integrations/github" className={classes.link}>
-                    <Icon name="github" className={classes.icon} />
-                    Github
-                </Link>
-                <Link to="/integrations/asana" className={classes.link}>
-                    <img name="asana" src={AsanaLogoBlack} className={classes.logo} />
-                    Asana
-                </Link>
-                <Link to="/integrations/trello" className={classes.link}>
-                    <img name="asana" src={TrelloLogoBlack} className={classes.logo} />
-                    Trello
-                </Link>
-                <Link to="/integrations/slack" className={classes.link}>
-                    <Icon name="slack" className={classes.icon} />
-                    Slack
-                </Link>
-                <Link to="/integrations/gmail" className={classes.link}>
-                    <Icon name="envelope" className={classes.icon} />
-                    Gmail
-                </Link>
+            <Flexbox name="integrations-container" grow={1} container="column" alignItems="center">
+                <IntegrationBox expand={location.query.expand} />
             </Flexbox>
         )
     }
 }
 
-const style = {
-    title: {
-        marginBottom: 15,
-        color: 'black',
-        fontSize: '1.4em'
-    },
-    link: {
-        color: 'black',
-        fontSize: '1.4em',
-        marginBottom: 15
-    },
-    icon: {
-        marginRight: 10
-    },
-    logo: {
-        marginRight: 10,
-        width:19,
-        height:18
-    },
-}
-
-export default useSheet(Integrations, style)
+export default Integrations
