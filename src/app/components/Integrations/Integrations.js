57cff373154e50997bbcab5076e8caf7a340df24
Daniel <daniel.wolf@freection.com>
2016-12-29 18:14:20 +0200

    231407833979379 Onboarding - welcome wizard
    Make the Integrations box more centralized in the app Integrations page.

--- src/app/components/Integrations/Integrations.js
+++ src/app/components/Integrations/Integrations.js
@@ -1,18 +1,25 @@
 import React, {Component} from 'react'
+import useSheet from 'react-jss'
 
 import Flexbox from '../UI/Flexbox'
 import IntegrationBox from './IntegrationsBox'
 
 class Integrations extends Component {
     render() {
-        const {location} = this.props
+        const {location, sheet: {classes}} = this.props
 
         return (
-            <Flexbox name="integrations-container" grow={1} container="column" alignItems="center">
+            <Flexbox name="integrations-container" grow={1} container="column" alignItems="center" className={classes.container}>
                 <IntegrationBox expand={location.query.expand} />
             </Flexbox>
         )
     }
 }
 
-export default Integrations
+const style = {
+    container: {
+        marginLeft: -170
+    }
+}
+
+export default useSheet(Integrations, style)
